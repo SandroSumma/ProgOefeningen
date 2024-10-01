732b6fb2997d31914e72f534c02d16120e68b416
SandroSumma <sandro.summa@student.ehb.be>
2024-10-01 11:27:04 +0200

    Oef2Hard klaar

---
+++ Module2/Hard/Oef2Hard.mjs
@@ -0,0 +1,15 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let jaar = parseFloat(await userInput.question("Voer een jaartal in:"));
+
+let dagenInFebruari = 28;
+
+if (jaar % 4 === 0 && (jaar % 100 !== 0 || jaar % 400 === 0)) {
+    dagenInFebruari = 29;
+}
+
+console.log("Februari in het jaar" + jaar + " heeft " + dagenInFebruari + " dagen.");
+
+process.exit();
